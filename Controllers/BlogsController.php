867bb80e8dea40979605f5deed3b47ed70b55363
Asaba William <asabawilliamdk@yahoo.com>
2023-11-21 06:35:09 +0300

    Update BlogsController

--- Controllers/BlogsController.php
+++ Controllers/BlogsController.php
@@ -2,28 +2,37 @@
 
 require 'Database.php';
 
-class BlogController
-{
+class BlogController {
 	public string $header;
 	public Database $db;
 
-	public function __construct()
-	{
+	public function __construct() {
 		$this->header = 'Our Blog';
 		$this->db = new Database();
 	}
 
-	public function index()
-	{
+	public function index() {
 		$posts = $this->db->all('blogs');
-
-		$post = $this->db->findOne('blogs', 'id', '=', '1');
-
 		require 'views/blogs/index.view.php';
 	}
 
+	public function show() {
+		$post = $this->db->findOne('blogs', 'title', '=', 'title 3');
+
+		require 'views/blogs/show.view.php';
+	}
+
+	public function create() {
+		$insert = $this->db->insert('blogs', [
+			'title' => 'Some tile',
+			'body' => 'some body here',
+		]);
+
+		require 'views/blogs/create.view.php';
+	}
 }
 
 $blogController = new BlogController();
 $posts = $blogController->index();
-
+$post = $blogController->show();
+$posts = $blogController->create();
