f0e01e0db8e3ab3d2594cb1910f72d3c5009c22b
Asaba William <asabawilliamdk@yahoo.com>
2023-11-21 06:35:52 +0300

    Add improvements to the DB Engine

--- Database.php
+++ Database.php
@@ -4,29 +4,45 @@ class Database {
 	public $connection;
 
 	public function __construct($username = 'root', $password = 'admin@2564') {
-
 		try {
 			$this->connection = new PDO('mysql:host=localhost;dbname=blog', $username, $password);
+			$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (PDOException $e) {
-			echo $e->getMessage();
+			echo "Connection failed: " . $e->getMessage();
 		}
 	}
 
 	public function all($table = null): array {
-		$statement = $this->connection->prepare("SELECT * FROM {$table}");
-
-		if (!is_null($statement)) {
+		try {
+			$statement = $this->connection->prepare("SELECT * FROM {$table}");
 			$statement->execute();
-			return $results = $statement->fetchAll();
+			return $statement->fetchAll();
+		} catch (PDOException $e) {
+			echo "Query failed: " . $e->getMessage();
+			return [];
 		}
 	}
 
 	public function findOne(string $table, string $column, string $operator, int | string $value): array {
-		$statement = $this->connection->prepare("SELECT * FROM {$table} WHERE {$column} {$operator} '{$value}'");
-
-		if (!is_null($statement)) {
+		try {
+			$statement = $this->connection->prepare("SELECT * FROM {$table} WHERE {$column} {$operator} :value");
+			$statement->bindParam(':value', $value);
 			$statement->execute();
-			return $result = $statement->fetch();
+			return $statement->fetch();
+		} catch (PDOException $e) {
+			echo "Query failed: " . $e->getMessage();
+			return [];
+		}
+	}
+
+	public function insert(string $table, $columns) {
+		try {
+			$column = implode(', ', array_keys($columns));
+			$value = "'" . implode("', '", array_values($columns)) . "'";
+			$statement = $this->connection->prepare("INSERT INTO {$table} ($column) VALUES ($value)");
+			$statement->execute();
+		} catch (PDOException $e) {
+			echo "Insert failed: " . $e->getMessage();
 		}
 	}
 }
