5734ab8b442c3ea932d345b28e3fd87656bad3f0
Asaba William <asabawilliamdk@yahoo.com>
2023-11-21 06:35:20 +0300

    wip

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,27 +1,4 @@
 <?php
+$header = 'Home';
 
-require 'Database.php';
-
-class HomeController
-{
-	public string $header;
-	public Database $db;
-
-	public function __construct()
-	{
-		$this->header = 'Home';
-		$this->db = new Database();
-	}
-
-
-	public function index()
-	{
-		$posts = $this->db->all('blogs');
-		require 'views/home/index.view.php';
-	}
-
-}
-
-$homeController = new HomeController();
-$posts = $homeController->index();
-
+require 'views/home/index.view.php';
